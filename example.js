4431375977f71ed057d1f5688263be5b79efea4f
Gourirane Yassine <yassine.gourirane@etudiant.univ-rennes1.fr>
2021-01-27 12:56:57 +0100

    read json file

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ var http = require ('http');
 var server = http.createServer(function (req, res){
   res.writeHead(200, {'Content-Type': 'application/json'});
   fs.createReadStream(file, 'utf8').pipe(res);
+      
   
 });
 server.listen(76);
