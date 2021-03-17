2e225ec9975f2088c570be5cbd97e5c6e6fafb64
anwar.machadi@gmail.com <anwar@100>
2021-03-17 13:32:12 +0100

    add Procfile

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const port = process.env.port || 8000
+
 var http = require("http");
 
 var methods = Object.create(null);
@@ -19,7 +19,7 @@ var server = http.createServer(function(request, response) {
     else
         respond(405, "Method " + request.method
             + " not allowed on " + request.url );
-}).listen(port);
+}).listen(process.env.PORT || 8000);
 console.log('Node server running on port 8000');
 
 function urlToPath(url) {
