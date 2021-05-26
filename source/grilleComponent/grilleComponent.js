2779871694976f243d09c9fb2f07815e83eaf0e1
Koko Kouadio Jean Yves Ol <kouadio.koko@etudiant.univ-rennes1.fr>
2021-05-27 00:03:20 +0200

    test

--- source/grilleComponent/grilleComponent.js
+++ source/grilleComponent/grilleComponent.js
@@ -1,19 +1,32 @@
 /**
  *
  */
+function handleChangeMesure() {
+    const tempo = $("#tempo").val();
+    const beat = $("#beat").val();
+    const armure = $("#armure").val();
+    const beat_mesure_time = $("#beat_mesure_time").val();
+    const divison_beat = $("#divison_beat").val();
 
+    const intensite = $("#intensite").val();
+    const alerte = $("#alerte").val();
 
-
+    console.log(tempo, beat, armure, divison_beat, beat_mesure_time, intensite, alerte , " value");
+}
 
 
 /**
  *
  */
-function handleClickSauvegarderMesure (){
+function handleClickSauvegarderMesure() {
     const _delai_demarrage = $("#delai_demarrage").val();
-    const _nombre_mesure= $("#nombre_mesure").val();
+    const _nombre_mesure = $("#nombre_mesure").val();
     const _titre_partition = $("#titre_partition").val();
     console.log('Handle input', _delai_demarrage, _nombre_mesure, _titre_partition);
 }
-$("#sauvegarder_mesures").click(handleClickSauvegarderMesure);
+
+$("#sauvegarder_mesures").click(()=>{
+    handleClickSauvegarderMesure();
+    handleChangeMesure();
+});
 
