858de072de8961c5ed4fb26a7654d41dafe20336
Wiem <wiem.nabli@etudiant.univ-rennes1.fr>
2021-06-02 14:59:15 +0200

    Hide and show list of sounds resolved

--- oscilateur/oscilateur.js
+++ oscilateur/oscilateur.js
@@ -113,6 +113,11 @@ function playEnd(theClock) {
 
 $("#chooserecord").change(function () {
     const want_mix =  $(this).get(0).checked;
-    console.log("Object est : ",  want_mix)
-    ;
+    if (want_mix == true)
+    {
+        document.getElementById("show-list-mix-file").style.display = "flex";
+    }
+    else
+        document.getElementById("show-list-mix-file").style.display = "none";
+
 });
